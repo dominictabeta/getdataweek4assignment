152607d93beee8478e43217297e35a05b5fab6f9
Dominic Tabeta <dominic.tabeta@gmail.com>
2016-04-23 16:10:17 +1000

    added more commentary to run_alaysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,33 +15,42 @@ subject_train <- read.table("train/subject_train.txt", sep = "")
 subject_test <- read.table("test/subject_test.txt", sep = "")
 
 readactivity <- read.table("activity_labels.txt", sep = "")
-activity <- as.character(readactivity$V2)
 
-# Merge the test and training into one dataframe
+
+
+# Merge the test and training data for X, Y and subject dataframes
 x_all<-rbind(x_train,x_test)
 y_all<-rbind(y_train,y_test)
 subject_all<-rbind(subject_train,subject_test)
 
 # Get the column numbers of only the measurements with 'mean' and 'std' in their names
 readmeasures <- read.table("features.txt", sep = "")
-#temp[grepl("mean",temp$V2) | grepl("std",temp$V2),]
+setwd( "..")
 
+# This code selects only the columns that have 'mean' or 'std' in the measure
+# name
 selectmeasures <- readmeasures[grepl("mean",readmeasures$V2) | grepl("std",readmeasures$V2),]
-
 x_select<-x_all[,selectmeasures$V1]
 
+# Apply the selected merasure names to the selected columns in X
 names(x_select)<-selectmeasures$V2
-#names(y_all)<-"activitycode"
+
+# Provide the "subject" column name
 names(subject_all)<-"subject"
 
+# This code  replaces the activty codes with descriptive activity names
 y_allnamed<-merge(x=y_all,y=readactivity,by.x="V1",by.y="V1",all.x=TRUE,sort=FALSE)
-
 names(y_allnamed)<-c("activitycode","activity")
 
+# dataframe 'combined' combines the X, Y, subject and activity columns into one
+# dataframe, and provides the column name 'activity' 
 combined<-cbind(subject_all,activity=y_allnamed[,-1],x_select)
 
+# dataframe 'final' is the dataframe of tidy data containing the mean of each 
+# measure for each subject-activity combination
 final<-summarise_each(group_by(combined,subject,activity),funs(mean))
 
+
+# This code writes the final tidy dataframe to the file 'tidy.txt"
 write.table(x = final,file = "tidydata.txt",row.name=FALSE)
 
-setwd( "..")
